50c7794e83310774c605731c7d16a9b525b9ef07
Jumana Schmidt <jumanarschmidt@gmail.com>
2022-08-12 12:35:27 -0500

    prog on rtplot

--- HandPlot.cpp
+++ HandPlot.cpp
@@ -7,26 +7,32 @@
 
 int main()
 {
-    morph::Visual v(1024, 768, "Continuous redrawing of GraphVisual");
+	morph::Visual v(1024, 768, "Continuous redrawing of GraphVisual");
 
-    auto gv = new morph::GraphVisual<double> (v.shaderprog, v.tshaderprog, {0,0,0});
+	auto gv = new morph::GraphVisual<double> (v.shaderprog, v.tshaderprog, {0,0,0});
 
-    morph::vVector<double> x;
-    x.linspace (-morph::mathconst<double>::pi, morph::mathconst<double>::pi, 100);
+	morph::vVector<double> x;
+	morph::vVector<double> y;
+	gv->setsize(2,2);
+	v.setSceneTransXY(-1, -1);
+	x.linspace (-morph::mathconst<double>::pi, morph::mathconst<double>::pi, 100);
+	y.linspace (-morph::mathconst<double>::pi, morph::mathconst<double>::pi, 100);
+	double dx = 0.;		
+	gv->setdata (x, y);
+	gv->finalize();
 
-    double dx = 0.0;
+	v.addVisualModel (gv);
 
-    gv->setdata (x, (x+dx).sin());
-    gv->finalize();
+	while (v.readyToFinish == false) {
+		dx += 0.01667;
+		glfwWaitEventsTimeout (0.01667); // 16.67 ms ~ 60 Hz
+		for(int i = 0; i < 100; i++)
+		{
+			y[i] = sin(x[i]+dx);
+		}
+		gv->update (x, y, 0);
+		v.render();
+	}
 
-    v.addVisualModel (gv);
-
-    while (v.readyToFinish == false) {
-        dx += 0.01;
-        glfwWaitEventsTimeout (0.01667); // 16.67 ms ~ 60 Hz
-        gv->update (x, (x+dx).sin(), 0);
-        v.render();
-    }
-
-    return 0;
+	return 0;
 }
